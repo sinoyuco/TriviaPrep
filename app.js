6dd4132372153780ec61baf9a63485817b80b1e4
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-11-01 13:13:11 -0500

    heroku build key setup

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const questions = require("./routes/api/questions");
 const bodyParser = require('body-parser');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 mongoose
     .connect(db, { useNewUrlParser: true })
